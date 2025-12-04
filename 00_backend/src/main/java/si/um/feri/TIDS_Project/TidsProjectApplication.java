28211ecd9da021a4ea7a5b14e83620b985eaea73
Kyuhi <Kyuhisan@gmail.com>
2025-12-04 11:04:58 +0100

    initial commit

---
+++ 00_backend/src/main/java/si/um/feri/TIDS_Project/TidsProjectApplication.java
@@ -0,0 +1,13 @@
+package si.um.feri.TIDS_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidsProjectApplication.class, args);
+	}
+
+}
---
+++ 00_backend/src/test/java/si/um/feri/TIDS_Project/TidsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package si.um.feri.TIDS_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
